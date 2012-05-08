353eec936e0381684c571e1deb9cf08408bd2300
unknown <dleitman@DLEITMAN-LAP7.qgil.micron.com>
2012-05-08 11:16:29 +0300

    New c# framework 4 version

---
+++ ForumServer/NetworkLayer/IForumListener.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace ForumServer.NetworkLayer
+{
+    /// <summary>
+    /// This interface is used for defining client object which gets callbacks from server
+    /// </summary>
+    public interface IForumListener
+    {
+        [OperationContract(IsOneWay = true)]
+        void onUpdate(string message, DateTime timestamp);
+        
+
+    }
+}
