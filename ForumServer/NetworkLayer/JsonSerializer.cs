65dffd43c7f83d15eadaa631e49a4e604c620ae2
sagioto <sagioto@gmail.com>
2012-05-08 20:06:32 +0300

    made serialize to limit recursion to 1 (replies to replies won't get serialized)

--- ForumServer/NetworkLayer/JsonSerializer.cs
+++ ForumServer/NetworkLayer/JsonSerializer.cs
@@ -14,6 +14,7 @@ namespace ForumServer.NetworkLayer
         public JsonSerializer()
         {
             serializer = new JavaScriptSerializer();
+            serializer.RecursionLimit = 1;
         }
 
 
