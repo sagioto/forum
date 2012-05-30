de1246bc8a2932286647c7825a710fa40f723c04
sagioto <sagioto@gmail.com>
2012-05-30 19:55:37 +0300

    made an awesome remove post

--- ForumServer/NetworkLayer/service-functions.js
+++ ForumServer/NetworkLayer/service-functions.js
@@ -6,6 +6,19 @@ var password;
 var currentPost = null;
 var currentSubforum = null;
 var recursionLevel = 1;
+var idCounter = 0;
+
+var NULL_VALUE = 0x0000;
+var OK = 0x0001;
+var USER_NOT_FOUND = 0x0002;
+var POST_NOT_FOUND = 0x0004;
+var SUB_FORUM_NOT_FOUND = 0x0010;
+var ENTRY_EXISTS = 0x0020;
+var INSUFFICENT_PERMISSIONS = 0x0040;
+var ADMIN_PERMISSIONS_NEEDED = 0x0100;
+var SECURITY_ERROR = 0x0200;
+var POLICY_REJECTED = 0x0400;
+var ILLEGAL_POST = 0x1000;
  
  
 function callService(methodName, params, onSuccess) {
@@ -57,7 +70,7 @@ function RegisterAndLoginCall(user, methodName){
 		function(result){
 		//alert("Result is: " + result[methodName + "Result"]);
 		switch(result[methodName + "Result"]){
-			case 1:
+			case OK:
 				if(methodName == "Register"){
 					RegisterAndLogin("Login");
 				}
@@ -73,14 +86,14 @@ function RegisterAndLoginCall(user, methodName){
 					});
 				}
 				break;
-			case 0:
-			case 2:if(methodName == "Login" & username != ""){
+			case NULL_VALUE:
+			case USER_NOT_FOUND:if(methodName == "Login" & username != ""){
 					alert("user " + username + " is not registered");
 					break;
 					} 
 				alert("user name and password cannot be empty");
 				break;
-			case 512: if(methodName == "Register"){
+			case SECURITY_ERROR: if(methodName == "Register"){
 					alert("user already exists");
 					}
 					else alert("user already logged in");
@@ -138,20 +151,20 @@ function ShowPosts(posts)
 			var tr = document.createElement("tr");
 			var td = document.createElement("td");
 			var post = posts[i];
-			
+			var id = idCounter++;
 			var buttons = "";
 			if(post.HasReplies)
-				buttons = '<button class="postButton" onclick="GetReplies(\'' + post.Key.Username + ',' + post.Key.Time + '\')" >view replies</button>';
+				buttons = '<button "class="postButton" onclick="GetReplies(\'' + post.Key.Username + ',' + post.Key.Time + '\')" >view replies</button>';
 			
 			if(username != "guest")
-				buttons = buttons + '<button class="postButton" onclick="showReply(\'' + post.Key.Username + ',' + post.Key.Time + '\')" >reply</button>'
-			+ '<button class="postButton" onclick="showEdit(\'' + post.Key.Username + ',' + post.Key.Time + '\')" >edit</button>'
-			+ '<button class="postButton" onclick="showRemove(\'' + post.Key.Username + ',' + post.Key.Time + '\')" >remove</button>';
+				buttons = buttons + '<button class="postButton" onclick="showReply(\'' + post.Key.Username + ',' + post.Key.Time + ',' + id + '\')" >reply</button>'
+			+ '<button class="postButton" onclick="showEdit(\'' + post.Key.Username + ',' + post.Key.Time + ',' + id + '\')" >edit</button>'
+			+ '<button class="postButton" onclick="Remove(\'' + post.Key.Username + ',' + post.Key.Time + ',' + id + '\')" >remove</button>';
 			
 			var buttonsTr = '<tr colspan="3"><td>' + buttons + '</td></tr>';
-			td.innerHTML = '<table width="800px"><tbody><tr><td class="postTitle">' + post.Title + '</td><td>' + 
+			td.innerHTML = '<div><table id="post' + id + '" width="800px"><tbody><tr><td class="postTitle">' + post.Title + '</td><td>' + 
 			'</td><td class="postPoster"> posted by ' + post.Key.Username + ' on ' + getDateString(post.Key.Time) + '</td></tr>' +
-			'<tr><td class="postContent" colspan="3"><h2>' + post.Body + '</h2></td></tr>' + buttonsTr + '</tbody></table>';
+			'<tr><td class="postContent" colspan="3"><h2>' + post.Body + '</h2></td></tr>' + buttonsTr + '</tbody></table></div>';
 			tr.appendChild(td);
 			td.setAttribute('class', 'post');
 			$('#subforumsTable').append(tr);
@@ -281,7 +294,16 @@ function Remove(postKey)
 	callService("RemovePost", {"postkey": { "Username" : splitted[0], "Time" : splitted[1]}, "username":username, "password":password},
 			function(result)
 			{
-				//TODO remove post from page
+				switch(result.RemovePostResult)
+				{
+					case OK:
+						var toRemove = '#post'+splitted[2];
+						$(toRemove).parent().slideUp('slow', function(){$(toRemove).parent().parent().remove();});
+						break;
+					default:
+						alert("insufficient permissions!");
+						break;
+				}
 			}
 	);
 }
