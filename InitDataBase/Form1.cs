a5fcfa6b3f02fc6747b65de4155850ce768efe6a
unknown <dleitman@DLEITMAN-LAP7.qgil.micron.com>
2012-06-02 15:09:01 +0300

    Data Base initalitor project - for cleaning all tables & initialize them

---
+++ InitDataBase/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ForumServer.DataLayer;
+
+namespace InitDataBase
+{
+    public partial class Form1 : Form
+    {
+        DataManager dm;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dm = new DataManager();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dm.CleanForumData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dm.InitForumData();
+        }
+
+    }
+}
