2073e28818b93b5cf080b9d6a51dea15d306643d
unknown <dleitman@DLEITMAN-LAP7.qgil.micron.com>
2012-06-02 14:13:17 +0300

    Client GUI WPF project was added - main functionallity works,still needs some UI upgrades

--- ForumClientWPF/StaticObjects.cs
+++ ForumClientWPF/StaticObjects.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ForumClientCore;
+using System.Windows;
 
 namespace ForumClientWPF
 {
     public static class StaticObjects
     {
-        public static PostWindow postWindow;
+        public static ClientController controller;
+        public static AddPostWin newPostWin;
+        public static Window darkwindow;
 
 
     }
